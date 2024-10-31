bf6929e42db30372297640ddbdfd199014727dc4
Art0riasss <guramravshan@gmail.com>
2024-10-31 09:52:43 +0300

    add all numbers

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
+
 let currentValue = 0
 let meth ='undefined'
+// number display
 document.getElementById('one').addEventListener('click', ()=>{
     document.getElementById('num_display').value += 1
+})
+document.getElementById('two').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 2
+})
+document.getElementById('three').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 3
+})
+document.getElementById('four').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 4
+})
+document.getElementById('five').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 5
+})
+document.getElementById('six').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 6
+})
+document.getElementById('seven').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 7
+})
+document.getElementById('eight').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 8
+})
+document.getElementById('nine').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 9
+})
+document.getElementById('zero').addEventListener('click', ()=>{
+    document.getElementById('num_display').value += 0
 })
 document.getElementById('plus').addEventListener('click', ()=>{
     meth = 'plus'
